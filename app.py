46d5a7703a3f17281468c054e2a9f65e4e9b3aee
Maidou Wang <maidou.wang@duke.edu>
2025-03-23 23:11:27 -0400

    M

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ from page import page1, page2, page3, page4
 if "page" not in st.session_state:
     st.session_state.page = 1
 
-page_container = st.empty()
-
 # Display the appropriate page in the container
 if st.session_state.page == 1:
-    page1(page_container)
+    page1()
 elif st.session_state.page == 2:
-    page2(page_container)
+    page2()
 elif st.session_state.page == 3:
-    page3(page_container)
+    page3()
 elif st.session_state.page == 4:
-    page4(page_container)
+    page4()
